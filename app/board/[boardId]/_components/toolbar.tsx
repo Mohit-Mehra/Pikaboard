899f62c96f72c3d5d20b7afbf36d4eab6243a5dd
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2024-03-15 12:07:32 +0530

    Liveblocks setup

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,4 @@
+
 export const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
@@ -11,6 +12,13 @@ export const Toolbar = () => {
         <div className="">Undo</div>
         <div className="">Redu</div>
       </div>
+    </div>
+  );
+};
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md">
     </div>
   );
 };
