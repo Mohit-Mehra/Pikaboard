df48992e75c3ab17afb0cdf51a6468f0dde35a01
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2024-03-17 20:18:51 +0530

    Participants

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user-avatar";
+
+const MAX_SHOWN_USERS = 2;
+
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      List of Users
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              src={info?.picture}
+              key={connectionId}
+              name={info?.name}
+              fallback={info?.name?.[0] || "T"}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
